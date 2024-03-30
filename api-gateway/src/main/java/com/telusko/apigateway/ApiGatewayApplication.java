32f525556ce5addde3e867b2276950cb39bb3c7e
J-sicard <ptiptilu@hotmail.fr>
2024-03-30 16:25:53 +0100

    Api-gateway ok

--- api-gateway/src/main/java/com/telusko/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/telusko/apigateway/ApiGatewayApplication.java
@@ -1,10 +1,9 @@
 package com.telusko.apigateway;
 
-import org.springframework.boot.SpringApplication;import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
