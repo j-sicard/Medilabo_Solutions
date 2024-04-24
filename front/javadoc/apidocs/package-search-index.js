1f164481b7be6b5c2ba661011f03ee61d57d8bb5
J-sicard <ptiptilu@hotmail.fr>
2024-04-24 17:38:38 +0200

    javadoc for diabetesmanagement front mpatient mcommentpatient created

---
+++ front/javadoc/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.mfront.front"},{"l":"com.mfront.front.beans"},{"l":"com.mfront.front.configuration"},{"l":"com.mfront.front.controller"},{"l":"com.mfront.front.proxies"}];updateSearchResults();
