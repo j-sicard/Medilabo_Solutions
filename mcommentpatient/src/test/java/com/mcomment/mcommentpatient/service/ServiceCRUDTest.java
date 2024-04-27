92094f25514df567d27de7ffd0aefa82cf21acb6
J-sicard <ptiptilu@hotmail.fr>
2024-04-27 16:25:28 +0200

    modif service

--- mcommentpatient/src/test/java/com/mcomment/mcommentpatient/service/ServiceCRUDTest.java
+++ mcommentpatient/src/test/java/com/mcomment/mcommentpatient/service/ServiceCRUDTest.java
@@ -1,17 +1,18 @@
 package com.mcomment.mcommentpatient.service;
-
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+
 import com.mcomment.mcommentpatient.McommentpatientApplication;
+
 
 @ExtendWith(SpringExtension.class)
 @ActiveProfiles("test")
 @SpringBootTest(classes = McommentpatientApplication.class)
 @TestPropertySource(locations = "classpath:application-test.properties")
-public class ServiceTest {
+public class ServiceCRUDTest {
 
 }
